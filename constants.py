770c1e8494f6dafd73cfbe221af1dbbf2fea9682
Sahil Yadav <99671469+Sahil481@users.noreply.github.com>
2024-01-23 18:49:12 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 BACKGROUND_FILE_PATH="background.jpeg"
 IMAGE_TAGS=["nature", "books", "aesthetic", "abstract", "dawn", "calm", "enviornment", "desert", "sky", "flowers", "sunrise", "sunset", "architecture", "city", "cities"]
 OUTPUT_FILE="output"
-INSTA_USERNAME="serene.phrases"
-INSTA_PASSWORD="Sy@8169940934"
+INSTA_USERNAME="Replace with your instagram username"
+INSTA_PASSWORD="Replace with your instagram password"
